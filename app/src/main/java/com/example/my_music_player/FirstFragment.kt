e4f7785037b47305c0d4c390426fc00a98568802
philippschoemacker <p.schoemacker@gmx.de>
2022-07-11 20:29:04 +0200

    Add basic music player functionality
    - Implement play and pause button
    - Implement seekbar
    - Add reset after song completion
    - Add test song as resource

--- app/src/main/java/com/example/my_music_player/FirstFragment.kt
+++ app/src/main/java/com/example/my_music_player/FirstFragment.kt
@@ -1,9 +1,13 @@
 package com.example.my_music_player
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import com.example.my_music_player.databinding.FragmentFirstBinding
 
@@ -16,6 +20,11 @@ class FirstFragment : Fragment() {
 
     // This property is only valid between onCreateView and on DestroyView.
     private val binding get() = _binding!!
+
+    private var updateSeekbar = false
+
+    private lateinit var runnable: Runnable
+    private lateinit var handler: Handler
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -30,10 +39,77 @@ class FirstFragment : Fragment() {
 
         // Set up UI for the fragment
         // Add listener for example
+
+        val mediaPlayer = this.createMediaPlayer()
+        this.createSeekBar(mediaPlayer)
+        this.addCompletionListener(mediaPlayer)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun createMediaPlayer(): MediaPlayer {
+        val mediaPlayer = MediaPlayer.create(activity, R.raw.music)
+        binding.playButton.setOnClickListener{
+            if(!mediaPlayer.isPlaying) {
+                mediaPlayer.start()
+                binding.playButton.setImageResource(R.drawable.ic_baseline_pause_circle_outline_24)
+
+                this.updateSeekbar = true
+                handler.postDelayed(runnable, 1000)
+            }
+            else
+            {
+                mediaPlayer.pause()
+                binding.playButton.setImageResource(R.drawable.ic_baseline_play_circle_outline_24)
+
+                this.updateSeekbar = false
+            }
+        }
+
+        return mediaPlayer
+    }
+
+    private fun createSeekBar(mediaPlayer: MediaPlayer) {
+        binding.seekbar.progress = 0
+        binding.seekbar.max      = mediaPlayer.duration
+
+        binding.seekbar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, position: Int, changed: Boolean) {
+                if(changed) {
+                    mediaPlayer.seekTo(position)
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+
+        this.runnable = Runnable {
+            if(mediaPlayer.isPlaying) {
+                binding.seekbar.progress = mediaPlayer.currentPosition
+            }
+
+            if(this.updateSeekbar) {
+                handler.postDelayed(runnable, 1000)
+            }
+        }
+
+        this.handler = Handler(Looper.getMainLooper())
+    }
+
+    private fun addCompletionListener(mediaPlayer: MediaPlayer) {
+        mediaPlayer.setOnCompletionListener {
+            binding.playButton.setImageResource(R.drawable.ic_baseline_play_circle_outline_24)
+            binding.seekbar.progress = 0
+
+            this.updateSeekbar = false
+
+            mediaPlayer.stop()
+            mediaPlayer.prepare()
+        }
     }
 }
